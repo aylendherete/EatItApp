5f24fc91a1a01da5cf536ba1fd89063886969c9a
Aylen Dherete <aylendherete@uca.edu.ar>
2024-06-24 17:54:57 -0300

    correccion

--- screens/index.jsx
+++ screens/index.jsx
@@ -2,7 +2,7 @@ import {Inicio} from "./Inicio";
 import { TipoRegistro } from "./TipoRegistro";
 import { RegistroNutricionista } from "./RegistroNutricionista";
 import { RegistroPaciente } from "./RegistroPaciente";
-import {ElegirNutricionista} from "./ElegirNutricionista";
+import {ElegirNutricionista} from "./elegirNutricionista";
 import {Paciente} from "./Paciente/Paciente";
 import {Nutricionista} from "./Nutricionista/Nutricionista"
 import { CartelSolicitud } from "./CartelSolicitud";
